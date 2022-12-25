98f42a3218dd2ad8c2c195ea5243bd21ce5e41bf
Elif Ozbey <eozbey2002@hotmail.com>
2022-12-25 09:20:13 -0800

    Hooks

---
+++ SpecflowBasics/Hooks/TestInitialize.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using TechTalk.SpecFlow;
+
+namespace SpecflowBasics.Hooks
+{
+    [Binding]
+    public sealed class TestInitialize
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        [BeforeTestRun]
+        public static void BeforeTest()
+        {
+            Console.WriteLine("This line executed before testrun");
+        }
+        [BeforeFeature]
+        public static void BeforeFeatureTest() => Console.WriteLine("This line executed before feature");
+
+        [BeforeScenario("mytag")]
+        public void BeforeScenarioWithTag()
+        {
+            Console.WriteLine("Before senerio execute this with tag");
+        }
+
+
+        [AfterScenario]
+        public void AfterScenario() => Console.WriteLine("This line executed after scenario");
+            
+    }
+}
